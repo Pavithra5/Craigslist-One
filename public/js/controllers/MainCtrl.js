60ded609f6e3a1330177f5ecbc56d1900d7c84df
abhishekk7 <abhishek3433kumar@gmail.com>
2016-11-22 21:37:11 -0600

    Changes to rearrange home page

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,31 +3,13 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $ht
 
 	$http.get('/api/categories/')
 	.success(function(data){
-		/*data = [ { _id: '582de3323153725a276269c8',
-    name: 'Housing',
-    subcategories:
-     [ { name: 'Apts/House', _id: 1 },
-       { name: 'Housing Swap', _id: 2 } ] },
-  { _id: '582de3683153725a276269c9',
-    name: 'Services',
-    subcategories:
-     [ { name: 'Something', _id: 1 },
-       { name: 'Something else', _id: 2 } ] } ];*/
-
+		$.each(data, function(index, value){
+			var count = 0;
+			value.length = value.subcategories.length;
+		});
        $scope.categories = data;
 	})
 	.error(function(data){
-		/*data = [ { _id: '582de3323153725a276269c8',
-    name: 'Housing',
-    subcategories:
-     [ { name: 'Apts/House', _id: 1 },
-       { name: 'Housing Swap', _id: 2 } ] },
-  { _id: '582de3683153725a276269c9',
-    name: 'Services',
-    subcategories:
-     [ { name: 'Something', _id: 1 },
-       { name: 'Something else', _id: 2 } ] } ];*/
-       
        console.log(data);
 	});
 

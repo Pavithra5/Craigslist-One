0a2f0c9bbc280add8bc8c1f58a4ff8a008a558b4
abhishekk7 <abhishek3433kumar@gmail.com>
2016-12-02 20:37:01 -0600

    Listing changes

--- public/js/controllers/ListingCtrl.js
+++ public/js/controllers/ListingCtrl.js
@@ -1,9 +1,22 @@
 // public/js/controllers/ClassifiedCtrl.js
 angular.module('ListingCtrl', []).controller('ListingController', function($scope, $http, $routeParams) {
 	$scope.homeUrl = window.location.origin;
-	$scope.category = "category";
-	$scope.subcategory = "subcategory";
-	console.log($routeParams);
+
+	$http.get('/api/categories/')
+	.success(function(data){
+		angular.forEach(data, function(value){
+			if(value._id == $routeParams.cid)
+			$scope.category = value.name;
+			angular.forEach(value.subcategories, function(subValue) {
+				if(subValue._id == $routeParams.sid)
+					$scope.subcategory = subValue.name;
+			});
+		});
+	})
+	.error(function(data){
+       console.log(data);
+	});
+
 
 	$http.get('/api/classified/show',{
 	    params: {

9fd84a054a9fad72086b7bacc6d436182fbbca48
Pavithra5 <kai5.pavi@gmail.com>
2016-12-07 01:39:30 -0600

    Separate call for pagination

--- app/routes.js
+++ app/routes.js
@@ -596,17 +596,364 @@ var passport = require('passport');
                     
                 }
 
-            },
+            }/*,
             {$sort: {shortdesc: 1}},
             {$skip:(req.query.page-1)*2},
-            {$limit: 2},
+            {$limit: 3},*/
             
 
 
                 ]).exec(function(err,result){
-                    res.json(result);
+
+                    
+                    console.log(result);
+                    
                     });
                     
+                    
+               
+
+
+                     });
+
+
+//To display an existing classified with pagination
+        app.get('/api/classified/showwithpage', function(req, res){
+        var filter={};
+        filter.isactive=1;
+        if(req.query.classified_id){
+            filter._id= new mongoose.Types.ObjectId(req.query.classified_id);
+        }
+        if(req.query.user_id){
+            filter.userid= new mongoose.Types.ObjectId(req.query.user_id);
+        }
+        
+        if(req.query.category_id) {
+            filter.catid = new mongoose.Types.ObjectId(req.query.category_id);
+        } 
+        if(req.query.subcategory_id) {
+            filter.subcatid = new mongoose.Types.ObjectId(req.query.subcategory_id);
+        }
+        if(req.query.post_date) {
+            filter.pdate = req.query.post_date;
+        }
+        if(req.query.update_date) {
+            filter.udate = req.query.update_date;
+        }
+        if(req.query.employmenttype){
+            filter.employmenttype=new mongoose.Types.ObjectId(req.query.employmenttype);
+        }
+        
+        if(req.query.conditionid){
+            filter.conditionid=new mongoose.Types.ObjectId(req.query.conditionid);
+        }
+        if(req.query.dateavailable){
+            filter.dateavailable=req.query.dateavailable;
+        }
+        if(req.query.bed){
+            filter.bed=parseInt(req.query.bed);
+        }
+        if(req.query.bath){
+            filter.bath=parseInt(req.query.bath);
+        }
+        if(req.query.housetype){
+            filter.housetype=new mongoose.Types.ObjectId(req.query.housetype);
+        }
+        if(req.query.laundry){
+            filter.laundry=parseInt(req.query.laundry);
+        }
+        if(req.query.parking){
+            filter.parking=parseInt(req.query.parking);
+        }
+        if(req.query.furnished){
+            filter.furnished=parseInt(req.query.furnished);
+        }
+        if(req.query.privateroom){
+            filter.privateroom=parseInt(req.query.privateroom);
+        }
+        if(req.query.privatebath){
+            filter.privatebath=parseInt(req.query.privatebath);
+        }
+        if(req.query.colors){
+            filter.paintcolor=new mongoose.Types.ObjectId(req.query.colors);
+        }
+        if(req.query.propulsion){
+            filter.propulsionid=new mongoose.Types.ObjectId(req.query.propulsion);
+        }
+        if(req.query.cylinder){
+            filter.cylinders=new mongoose.Types.ObjectId(req.query.cylinder);
+        }
+        if(req.query.drivetype){
+            filter.drivetype=new mongoose.Types.ObjectId(req.query.drivetype);
+        }
+        if(req.query.fueltype){
+            filter.fueltype=new mongoose.Types.ObjectId(req.query.fueltype);
+        }
+        if(req.query.size){
+            filter.sizeid=new mongoose.Types.ObjectId(req.query.size);
+        }
+        if(req.query.titlestatus){
+            filter.titlestatus=new mongoose.Types.ObjectId(req.query.titlestatus);
+        }
+        if(req.query.transmission){
+            filter.transmissionid=new mongoose.Types.ObjectId(req.query.transmission);
+        }
+        if(req.query.vehicletype){
+            filter.vehicletype=new mongoose.Types.ObjectId(req.query.vehicletype);
+        }
+        if(req.query.make){
+            filter.make=req.query.make;
+        }
+        if(req.query.model){
+            filter.model=req.query.model;
+        }
+        if(req.query.dealer){
+            filter.dealer=parseInt(req.query.dealer);
+        }
+        if(req.query.area1){
+            filter.area= { $gt: parseInt(req.query.area1), $lt: parseInt(req.query.area2) }; 
+        }
+        if(req.query.price1){
+            filter.price= { $gt: parseInt(req.query.price1), $lt: parseInt(req.query.price2) };
+        }
+        if(req.query.width1){
+            filter.dwidth= { $gt: parseInt(req.query.width1), $lt: parseInt(req.query.width2) }; 
+        }
+        if(req.query.height1){
+            filter.dheight= { $gt: parseInt(req.query.height1), $lt: parseInt(req.query.height2) }; 
+        }
+        if(req.query.length1){
+            filter.dlength= { $gt: parseInt(req.query.length1), $lt: parseInt(req.query.length2) }; 
+        }
+        if(req.query.year1){
+            filter.year= { $gt: parseInt(req.query.year1), $lt: parseInt(req.query.year2) }; 
+        }
+        if(req.query.odometer1){
+            filter.odometer= { $gt: parseInt(req.query.odometer1), $lt: parseInt(req.query.odometer2) }; 
+        }
+       
+        if(req.query.search){
+            filter.$or= [
+                        {desc:{$regex :new RegExp(".*"+req.query.search+".*","i")}},
+                        {shortdesc:{$regex :new RegExp(".*"+req.query.search+".*","i")}}
+
+                            
+                      ];
+        }
+        var count=getCount();
+        function getCount()
+        {
+            return Classified.count(filter).exec();
+        }
+        count.then(function(counter){
+            Classified.aggregate([
+            
+            { 
+                $match:
+               
+                    filter
+          
+            },
+            {
+                $lookup:
+                {
+                    from: "users",
+                    localField: "userid",
+                    foreignField: "_id",
+                    as: "user",
+                    
+                    
+                }
+               
+            },
+            {
+                 $lookup:
+                {
+                    from: "category",
+                    localField: "catid",
+                    foreignField: "_id",
+                    as: "categ",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "subcategory",
+                    localField: "subcatid",
+                    foreignField: "_id",
+                    as: "subcateg",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "condition",
+                    localField: "conditionid",
+                    foreignField: "_id",
+                    as: "condition",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "transmission",
+                    localField: "transmissionid",
+                    foreignField: "_id",
+                    as: "transmission",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "fueltype",
+                    localField: "fueltype",
+                    foreignField: "_id",
+                    as: "fuel",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "housingtype",
+                    localField: "housetype",
+                    foreignField: "_id",
+                    as: "housetype",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "cylinders",
+                    localField: "cylinders",
+                    foreignField: "_id",
+                    as: "cylinders",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "drivetype",
+                    localField: "drivetype",
+                    foreignField: "_id",
+                    as: "drive",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "colors",
+                    localField: "paintcolor",
+                    foreignField: "_id",
+                    as: "paint",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "titlestatus",
+                    localField: "titlestatus",
+                    foreignField: "_id",
+                    as: "titlestatus",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "vehicletype",
+                    localField: "vehicletype",
+                    foreignField: "_id",
+                    as: "vehicletype",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "employmenttype",
+                    localField: "employmenttype",
+                    foreignField: "_id",
+                    as: "employmenttype",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "size",
+                    localField: "sizeid",
+                    foreignField: "_id",
+                    as: "vehiclesize",
+                    
+                    
+                }
+
+            },
+            {
+                 $lookup:
+                {
+                    from: "propulsion",
+                    localField: "propulsionid",
+                    foreignField: "_id",
+                    as: "propulsion",
+                    
+                    
+                }
+
+            },
+            {$sort: {shortdesc: 1}},
+            {$skip:0*2},
+            {$limit: 3}
+            
+
+
+                ]).exec(function(err,result){
+
+                    
+                    var totalresult={};
+                    totalresult.count=counter;
+                    totalresult.result=result;
+                    res.json(totalresult);
+                    });
+                    
+
+
+        })
+
+        
                     
                
 
